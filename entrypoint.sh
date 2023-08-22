15553f3fb5f2a4bd1850fd2c605cc29c66f4760e
Erik Karsten <erik@opcode.xyz>
2023-08-22 16:08:25 +0300

    fix dir creation, don't delete .gv file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 cd "$GITHUB_WORKSPACE" || exit
-mkdir -p "$GITHUB_WORKSPACE/$GRAPH_OUTPUT_DIR"
+mkdir -p "$(dirname "$GRAPH_OUTPUT_DIR")"
 
 GRAPH_OUTPUT=$(echo "$GRAPH_OUTPUT" | sed 's/\.[^.]*$//') # Remove extension from the file name
 
 go-callvis -file "$GRAPH_OUTPUT" -format png $GRAPH_ARGS
-rm "$GRAPH_OUTPUT.gv"
