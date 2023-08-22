5d525c11ec4fd4e1b7c4c434ce01e9921691813d
Erik Karsten <erik@opcode.xyz>
2023-08-22 15:55:19 +0300

    cleanup go-callvis generated files

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 cd "$GITHUB_WORKSPACE" || exit
 mkdir -p "$GITHUB_WORKSPACE/$GRAPH_OUTPUT_DIR"
+
+GRAPH_OUTPUT=$(echo "$GRAPH_OUTPUT" | sed 's/\.[^.]*$//') # Remove extension from the file name
+
 go-callvis -file "$GRAPH_OUTPUT" -format png $GRAPH_ARGS
+rm "$GRAPH_OUTPUT.gv"
